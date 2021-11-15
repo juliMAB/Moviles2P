17a170559c7c4d99d35d233e4f2ee5c6bba505fe
juliMAB <julian6aguirre@hotmail.com>
2021-11-14 21:14:43 -0300

    Added google play plugins.

---
+++ MovileP2/Assets/scripts/GooglePlay/GetAchivement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAchivement : MonoBehaviour
+{
+    public void crazyFunk()
+    {
+        PlayGamesScript.UnlockAchievement(GPGSIds.achievement_abrir_el_juego);
+        PlayGamesScript.ShowAchievementsUI();
+    }
+    public void ShowAUI()
+    {
+        PlayGamesScript.ShowAchievementsUI();
+    }
+}
---
+++ MovileP2/Assets/scripts/GooglePlay/PlayGamesScript.cs
@@ -0,0 +1,43 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class PlayGamesScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        SingIn();
+    }
+
+    // Update is called once per frame
+    void SingIn()
+    {
+        Social.localUser.Authenticate(success => { });
+    }
+
+    #region Achievements
+    public static void UnlockAchievement(string id)
+    {
+        Social.ReportProgress(id, 100, success => { });
+    }
+
+    public static void ShowAchievementsUI()
+    {
+        Social.ShowAchievementsUI();
+    }
+    #endregion /Achievements
+    #region Leaderboards
+    public static void AddScoreToLeaderboard(string leaderboardId, long score)
+    {
+        Social.ReportScore(score, leaderboardId, success => { });
+    }
+    public static void ShowLeaderboardsUI()
+    {
+        Social.ShowLeaderboardUI();
+    }
+    #endregion /Leaderboards
+}
