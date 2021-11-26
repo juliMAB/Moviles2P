a0dd6d668208582587d56aa0f5d0094c79c586da
juliMAB <julian6aguirre@hotmail.com>
2021-11-26 01:48:03 -0300

    Add plugin to Write and Read.

--- MovileP2/Assets/scripts/JLogger.cs
+++ MovileP2/Assets/scripts/JLogger.cs
@@ -8,22 +8,79 @@ public class JLogger
 
     const string LOGGER_CLASS_NAME = "JLogger";
     
+    const string FILEMANAGER_CLASS_NAME = "FileManager";
+    
     static AndroidJavaClass JLoggerClass=null;
 
     static AndroidJavaObject JLoggerInstance = null;
 
+    static AndroidJavaClass FileManagerClass = null;
+
+    static AndroidJavaObject FileManagerInstance = null;
+
+    
+
 
     static void init()
     {
+#if UNITY_ANDROID && !UNITY_EDITOR
         JLoggerClass = new AndroidJavaClass(PACK_NAME + "." + LOGGER_CLASS_NAME);
         JLoggerInstance = JLoggerClass.CallStatic<AndroidJavaObject>("GetInstance");
+        FileManagerClass = new AndroidJavaClass(PACK_NAME + "." + FILEMANAGER_CLASS_NAME);
+        FileManagerInstance = FileManagerClass.CallStatic<AndroidJavaObject>("GetInstance");
+
+        AndroidJavaClass playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        FileManagerClass.SetStatic<AndroidJavaObject>("myActivity", activity);
+#endif
     }
     public static void SendLog(string msg)
     {
+#if UNITY_ANDROID && !UNITY_EDITOR  
         if (JLoggerInstance==null)
         {
             init();
         }
         JLoggerInstance.Call("SendLog", msg);
+        WriteFile(msg);
+#endif
     }
+    public static string ReadFile()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (FileManagerInstance == null)
+        {
+            init();
+        }
+        return FileManagerInstance.CallStatic<string>("ReadFile", " ");
+#else
+        return "";
+#endif
+    }
+
+    private static void WriteFile(string msg)
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (FileManagerInstance == null)
+        {
+            init();
+        }
+        FileManagerInstance.CallStatic("WriteFile", msg);
+#endif
+    }
+
+
+    public static void ClearFile()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (FileManagerInstance == null)
+        {
+            init();
+        }
+        FileManagerInstance.CallStatic("ClearFile", " ");
+#endif
+    }
+
+
+
 }
--- MovileP2/JuliPlugins/JuliLogger2/src/main/java/com/example/julilogger2/AlertViewCaller.java
+++ MovileP2/JuliPlugins/JuliLogger2/src/main/java/com/example/julilogger2/AlertViewCaller.java
@@ -1,8 +1,36 @@
 package com.example.julilogger2;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class AlertViewCaller {
 
+    public  static  final FileManager _instance = new FileManager();
 
+    public  static  FileManager GetInstance(){
+        return _instance;
+    }
+
+    public static Activity myActivity;
+
+    public static void WriteFile(String a){
+        Context mycontext = myActivity.getApplicationContext();
+        try {
+            OutputStreamWriter outputStreamWriter= new OutputStreamWriter(mycontext.openFileOutput("logs", Context.MODE_APPEND));
+
+            outputStreamWriter.write(a + "\n");
+            outputStreamWriter.close();
+
+            Log.d("JL=>", "Wrote succesfully");
+        }
+        catch (IOException e) {
+            Log.e("JL=>", "error: " +e.toString());
+        }
+    }
 
 }
