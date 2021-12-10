14a5152d21fb4386959b52aec5c6366426ccbe6c
juliMAB <julian6aguirre@hotmail.com>
2021-12-10 04:49:20 -0300

    New FB functions Logs, share.

--- MovileP2/Assets/scripts/Facebook/FBAdsManager.cs
+++ MovileP2/Assets/scripts/Facebook/FBAdsManager.cs
@@ -13,6 +13,8 @@ public class FBAdsManager : MonoBehaviour
     private void Start()
     {
         LoadBanner();
+
+        LoadInterstitial();
     }
     public void LoadBanner()
     {
@@ -21,7 +23,7 @@ public class FBAdsManager : MonoBehaviour
             this.adView.Dispose();
         }
 
-        this.adView = new AdView("IMG_16_9_APP_INSTALL#your-placement-id", AdSize.BANNER_HEIGHT_50);
+        this.adView = new AdView("IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID", AdSize.BANNER_HEIGHT_50);
         this.adView.Register(this.gameObject);
 
         // Set delegates to get notified on changes or when the user interacts with the ad.
@@ -45,7 +47,7 @@ public class FBAdsManager : MonoBehaviour
 
     public void LoadInterstitial()
     {
-        this.interstitialAd = new InterstitialAd("YOUR_PLACEMENT_ID");
+        this.interstitialAd = new InterstitialAd("IMG_16_9_LINK#YOUR_PLACEMENT_ID");
         this.interstitialAd.Register(this.gameObject);
 
         // Set delegates to get notified on changes or when the user interacts with the ad.
